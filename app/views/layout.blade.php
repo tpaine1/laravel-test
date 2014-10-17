f0e9189962690fe5c47ea121d9a662a5d137d876
Thomas Paine <tpaine@kineticsupply.com>
2014-10-17 15:35:57 -0500

    added table columns and changed width of columns to 25% each

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -5,12 +5,14 @@
             <div style="margin: auto;">
                 <table style="width: 100%; text-align: center;">
                     <tr>
-                        <td style="width: 50%;">
+                        <td style="width: 25%;"></td>
+                        <td style="width: 25%;">
                             <h3>Name</h3>
                         </td>
-                        <td style="width: 50%;">
+                        <td style="width: 25%;">
                             <h3>Email</h3>
                         </td>
+                        <td style="width: 25%;"></td>
                     </tr>
                 </table>
             </div>
