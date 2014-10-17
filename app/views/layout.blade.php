138b05f35f3af15b919ce71748a7d622310bf7b8
Thomas Paine <tpaine@kineticsupply.com>
2014-10-17 15:26:40 -0500

    added div to center text on users view

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,16 +1,18 @@
 <html>
     <body>
-        <h1>Users List</h1>
-        <table>
-            <tr>
-                <td style="width: 100px;">
-                    <h3>Name</h3>
-                </td>
-                <td style="width: 100px;">
-                    <h3>Email</h3>
-                </td>
-            </tr>
-        </table>
-        @yield('content')
+        <div style="text-align: center;">
+            <h1>Users List</h1>
+            <table>
+                <tr>
+                    <td style="width: 100px;">
+                        <h3>Name</h3>
+                    </td>
+                    <td style="width: 100px;">
+                        <h3>Email</h3>
+                    </td>
+                </tr>
+            </table>
+            @yield('content')
+        </div>
     </body>
 </html>
