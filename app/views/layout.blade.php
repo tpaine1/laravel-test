24dcb4a40df50eb0eebafa0fa8bf56eb8022eb27
Thomas Paine <tpaine@kineticsupply.com>
2014-10-17 15:34:19 -0500

    increased table width to 100%

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -3,12 +3,12 @@
         <div style="text-align: center;">
             <h1>Users List</h1>
             <div style="margin: auto;">
-                <table style="width: 100%">
+                <table style="width: 100%; text-align: center;">
                     <tr>
-                        <td style="width: 100px;">
+                        <td style="width: 50%;">
                             <h3>Name</h3>
                         </td>
-                        <td style="width: 100px;">
+                        <td style="width: 50%;">
                             <h3>Email</h3>
                         </td>
                     </tr>
